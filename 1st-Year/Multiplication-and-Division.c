98ea7d48a39b1c5fd8de391258f88ee96dd76ca6
Berat Tezer <64587561+Beratezer@users.noreply.github.com>
2022-10-14 12:17:42 +0300

    Upload 10.14.22
    Operations in C

---
+++ 1st-Year/Multiplication-and-Division.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+  double a;
+  double b;
+  double c;
+
+  a = 3.0 / 4.0 / 5.0;
+  b = 3.0 / (4.0 / 5.0);
+  c = 6.0 / 2.0 * 3.0;
+
+  printf ("%lf\n", a);
+  printf ("%lf\n", b);
+  printf ("%lf", c);
+}
