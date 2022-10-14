4ff88ba05c844285a1e6285beeaec3cba8d5fa8f
Berat Tezer <64587561+Beratezer@users.noreply.github.com>
2022-10-14 14:27:33 +0300

    Upload 14.10.22
    Conditions in C

---
+++ Conditions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main () {
+  int a = 15;
+
+  _Bool x;
+
+  x = a >= 10 && a <= 20;
+
+  printf("%d\n", x);
+  
+  a = 20;
+  int b = 10;
+  
+  _Bool y;
+  
+  y = (a + b) >= 20 || (a - b) <= 10;
+  
+  printf("%d", y);
+}
