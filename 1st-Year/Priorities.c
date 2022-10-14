546bd18fb0ed95f0e5f0f1a5a6c33412680ca6cc
Berat Tezer <64587561+Beratezer@users.noreply.github.com>
2022-10-14 14:40:24 +0300

    Upload 10.14.22
    Operation priorities in C

---
+++ 1st-Year/Priorities.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main () {
+    int a = 20;
+    int b = 10;
+    int c = 15;
+    int d = 5;
+    
+    double e;
+    double f;
+    double g;
+    double h;
+    
+    e = (a + b) * c / d;
+    f = ((a + b) * c) / d;
+    g = (a + b) / (c / d);
+    h = a + (b + c) / d;
+    
+    printf("%lf \n", e);
+    printf("%lf \n", f);
+    printf("%lf \n", g);
+    printf("%lf", h);
+}
