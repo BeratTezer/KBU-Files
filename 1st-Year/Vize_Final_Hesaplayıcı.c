393fc47ca37a598f37c6210e4f0f5244f5f79106
Berat Tezer <64587561+Beratezer@users.noreply.github.com>
2022-10-14 11:31:36 +0300

    Create Vize_Final_Hesaplayıcı.c
    Upload 10.14.22

---
+++ 1st-Year/Vize_Final_Hesaplayıcı.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main() {
+    float vize, final, sonuc;
+
+    printf("Bu uygulama vize ve final notunu alarak ders durumunu rapor eder.\n");
+	
+    printf("Vize notunuzu giriniz > ");
+    scanf("%f", &vize);
+	
+	printf("Final notunuzu giriniz > ");
+    scanf("%f", &final);
+	
+	vize = vize * 0.4;
+	final = final * 0.6;
+	sonuc = vize + final;
+	
+    if ((vize+final) > 60) {
+        printf("\nGeçtiniz, notunuz: %f", sonuc);
+    }
+    else {
+        printf("\nKaldınız, notunuz: %f", sonuc);
+    }
+}
