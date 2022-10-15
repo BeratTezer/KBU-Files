88e7dfa32788f45d973e85051a2d87ef04de307a
Berat Tezer <64587561+Beratezer@users.noreply.github.com>
2022-10-15 21:04:50 +0300

    Update 4-Hexadecimal-Number-Converter.c

--- 1st-Year/4-Hexadecimal-Number-Converter.c
+++ 1st-Year/4-Hexadecimal-Number-Converter.c
@@ -5,6 +5,11 @@ void main()
     _Bool loopController = 0;
     int typeSelection;
     int userNumber;
+    char* digit1;
+    char* digit2;
+    char* digit3;
+    char* digit4;
+    char* digit5;
     
     int length(unsigned x) {
     if (x >= 10000)      return 5;
@@ -12,6 +17,25 @@ void main()
     if (x >= 100)        return 3;
     if (x >= 10)         return 2;
     return 1;
+    }
+    
+    char* converter16(unsigned x) {
+    if (x == 15)         return "F";
+    if (x == 14)         return "E";
+    if (x == 13)         return "D";
+    if (x == 12)         return "C";
+    if (x == 11)         return "B";
+    if (x == 10)         return "A";
+    if (x == 9)          return "9";
+    if (x == 8)          return "8";
+    if (x == 7)          return "7";
+    if (x == 6)          return "6";
+    if (x == 5)          return "5";
+    if (x == 4)          return "4";
+    if (x == 3)          return "3";
+    if (x == 2)          return "2";
+    if (x == 1)          return "1";
+    return "0";
     }
     
     printf("This program will help you in converting decimal and hexadecimal numbers to each other.\n\n");
@@ -25,14 +49,26 @@ void main()
             scanf("%d", &userNumber);
             
             if (length(userNumber) == 1) {
-                
+                printf("%s", converter16(userNumber));
             } else if (length(userNumber) == 2) {
-                
+                if (userNumber <= 16) {
+                    printf("%s", converter16(userNumber));
+                } else {
+                    digit2 = converter16(userNumber/16);
+                    digit1 = converter16(userNumber - (userNumber/16)*16);
+                    printf("\n\nThe result is: %s%s\n\n", digit2, digit1);
+                }
             } else if (length(userNumber) == 3) {
+                
+                
                 
             } else if (length(userNumber) == 4) {
                 
+                
+                
             } else if (length(userNumber == 5)) {
+                
+                
                 
             } else {
                 printf("\n\nYou have to write a number.\n\n");
