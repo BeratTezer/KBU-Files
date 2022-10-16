5407e336849562b5b3436a6ead5eda5dceff5d17
Berat Tezer <64587561+Beratezer@users.noreply.github.com>
2022-10-16 15:47:45 +0300

    Upload 10.16.22
    Number System Converter in C

--- 1st-Year/4-Hexadecimal-Number-Converter.c
+++ 1st-Year/4-Hexadecimal-Number-Converter.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 void main()
 {
@@ -12,11 +13,12 @@ void main()
     char* digit5;
     
     int length(unsigned x) {
-    if (x >= 10000)      return 5;
-    if (x >= 1000)       return 4;
-    if (x >= 100)        return 3;
-    if (x >= 10)         return 2;
-    return 1;
+        if (x > 99999)       return 0;
+        if (x >= 10000)      return 5;
+        if (x >= 1000)       return 4;
+        if (x >= 100)        return 3;
+        if (x >= 10)         return 2;
+        return 1;
     }
     
     char* converter16(unsigned x) {
@@ -38,14 +40,14 @@ void main()
     return "0";
     }
     
-    printf("This program will help you in converting decimal and hexadecimal numbers to each other.\n\n");
+    printf("This program will help you in converting decimal numbers to hexadecimal numbers.\n\n");
     
     while (loopController == 0) {
-        printf("Please select convertation type: \n\n 1- Decimal to hexadecimal \n 2- Hexadecimal to decimal \n\n 0- Exit \n\n> ");
+        printf("Please select convertation type: \n\n 1- Decimal to hexadecimal \n\n 0- Exit \n\n> ");
         scanf("%d", &typeSelection);
         
         if (typeSelection == 1) {
-            printf("\n\nPlease enter your decimal number, it will convert to hexadecimal (Max 5 digits)\n\n> ");
+            printf("\n\nPlease enter your decimal number, it will convert to hexadecimal (Max 5 digits)\n\n#> ");
             scanf("%d", &userNumber);
             
             if (length(userNumber) == 1) {
@@ -98,30 +100,22 @@ void main()
                     printf("\n\nThe result is: %s%s%s%s%s\n\n", digit5, digit4, digit3, digit2, digit1);
                 }
             } else {
-                printf("\n\nYou have to write a number.\n\n");
+                printf("\n\nYou have to write a number under six digit.\n\n");
                 break;
             }
-            
-            
-            
-        } else if (typeSelection == 2) {
-            
-            
-            
-            
-            
-        } else if (typeSelection == 0) {
-            printf("Program is closing...");
+        }  else if (typeSelection == 0) {
+            printf("\n\nProgram is closing...\n");
+            sleep(2);
             break;
         } else {
             printf("\n\nUndefinied input\n\n");
+            sleep(3);
             break;
         }
         
-        
-        
-        // 
-        break;
+        sleep(3);
+        printf("\n");
+        continue;
     }
 
 }
