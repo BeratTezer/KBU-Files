53c919556c2628291380467a62ddc6e6a64ae6db
Berat Tezer <64587561+Beratezer@users.noreply.github.com>
2022-10-29 10:35:29 +0300

    Upload 10.29.22
    Controlled Loop 1 C file

---
+++ 1st-Year/5-Controlled-Loop1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int grade, totalGrade=0, counter=0;
+    
+    while (1) {
+        if (counter<10) {
+            printf("Please write a grade below;\nGrade: ");
+            scanf("%d", &grade);
+            totalGrade+=grade;
+            counter++;
+        }
+        else {
+            printf("\n\nAverage: %d", totalGrade/counter);
+            break;
+        }
+    }
+
+}
