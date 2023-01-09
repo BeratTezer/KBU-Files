64715b878b843a8ed119c8022edf9e1ccfc8fc11
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2023-01-09 12:40:45 +0300

    23.1.9 Upload
    Homework - Done

--- 1st-Year/Programming-Languages-l/30-Improved-Matrix-Filter.c
+++ 1st-Year/Programming-Languages-l/30-Improved-Matrix-Filter.c
@@ -12,14 +12,21 @@ int main() {
     scanf("%d", &N);
     
     // Bu degiskenlerin kullanilmasi ile matrislerin tanimlanması
-    int randomMatris[M-1][N-1], controlMatris[3][3], resultMatris[M-1][N-1];
+    int randomMatris[M][N], newRandomMatris[M+2][N+2], controlMatris[3][3], resultMatris[M-1][N-1];
     
     // Matrisi 0-255 arasindaki rastgele degerler ile doldurma
     for (i = 0 ; i <= M-1 ; i++) {
         for (j = 0 ; j <= N-1 ; j++) {
             randomMatris[i][j] = rand() % 256;
-            // Result matris kendi kendine deger atıyordu, bu sekilde engelledim
             resultMatris[i][j]=0;
+        }
+    }
+    
+    // Matrisi 0 degerleri ile cevreleme (otomatik adaptasyon saglaması icin)
+    for (i = 0 ; i <= M+2 ; i++) {
+        for (j = 0 ; j <= N+2 ; j++) {
+            newRandomMatris[i][j] = 0;
+            if (i != 0 && j != 0 && i != M+1 && j != N+1) newRandomMatris[i][j] = randomMatris[i-1][j-1];
         }
     }
     
@@ -31,12 +38,10 @@ int main() {
         }
     }
     
-    // Kontrol matrisi ile filtreleme
-    for (i = 0 ; i <= M-1 ; i++) {
-        for (j = 0 ; j <= N-1 ; j++) {
-            if (i+2 <= M-1 && j+2 <= N-1) {
-                resultMatris[i+1][j+1] = randomMatris[i][j] * controlMatris[0][0] + randomMatris[i][j+1] * controlMatris[0][1] + randomMatris[i][j+2] * controlMatris[0][2] + randomMatris[i+1][j] * controlMatris[1][0] + randomMatris[i+1][j+1] * controlMatris[1][1] + randomMatris[i+1][j+2] * controlMatris[1][2] + randomMatris[i+2][j] * controlMatris[2][0] + randomMatris[i+2][j+1] * controlMatris[2][1] + randomMatris[i+2][j+2] * controlMatris[2][2];
-            }
+    // Kontrol matrisi ile yeni rastgele matrisi filtreleme (burada artik tum degerler filtrelenmis olacak cunku kontrol edilecek matrisi sifirlar ile cevreledik)
+    for (i = 0 ; i <= M+1 ; i++) {
+        for (j = 0 ; j <= N+1 ; j++) {
+            resultMatris[i][j] = newRandomMatris[i][j] * controlMatris[0][0] + newRandomMatris[i][j+1] * controlMatris[0][1] + newRandomMatris[i][j+2] * controlMatris[0][2] + newRandomMatris[i+1][j] * controlMatris[1][0] + newRandomMatris[i+1][j+1] * controlMatris[1][1] + newRandomMatris[i+1][j+2] * controlMatris[1][2] + newRandomMatris[i+2][j] * controlMatris[2][0] + newRandomMatris[i+2][j+1] * controlMatris[2][1] + newRandomMatris[i+2][j+2] * controlMatris[2][2];
         }
     }
     
