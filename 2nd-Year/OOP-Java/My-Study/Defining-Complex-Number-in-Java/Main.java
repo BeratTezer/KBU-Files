f90d1a651b057e814fbd377a4b7686da7a114783
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2023-10-17 14:20:30 +0300

    Upload 17.10.23

---
+++ 2nd-Year/OOP-Java/My-Study/Defining-Complex-Number-in-Java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        complex number1 = new complex();
+        number1.real = 10;
+        number1.imaginary = 5;
+
+        System.out.printf("Number is %d + %di", number1.real, number1.imaginary);
+    }
+}
---
+++ 2nd-Year/OOP-Java/My-Study/Defining-Complex-Number-in-Java/complex.java
@@ -0,0 +1,6 @@
+public class complex {
+    public int real;
+    public int imaginary;
+}
+
+
