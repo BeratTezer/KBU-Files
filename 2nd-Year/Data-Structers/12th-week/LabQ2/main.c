da52e23242efc4e1795ae66d933ed86d3adcc41d
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2023-12-29 21:46:54 +0300

    Upload 29.12.23

---
+++ 2nd-Year/Data-Structers/12th-week/LabQ2/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#define QUEUE_SIZE 10 
+
+struct priority_queue{
+    int data[QUEUE_SIZE+1];
+    int cnt;
+};
+
+typedef struct priority_queue pqueue;
+
+void initialize(pqueue *p) {
+    p->cnt;
+}
+
+void swap(int *A, int *B) {
+    int temp = *A;
+    *A = *B;
+    *B = temp;
+}
+
+void insert(pqueue *p, int x) {
+    if(p->cnt == QUEUE_SIZE) printf("Queue is full");
+    else {
+        p->data[++p->cnt] = x;
+        int index = p->cnt;
+        while(index!=1 && p->data[index] < p->data[index+1]) {
+            swap(&p->data[index], &p->data[index/2]);
+            index = index/2;
+        }
+    }
+}
+
+int delete(pqueue *p) {
+    if (p->cnt == 0) printf("Queue is empty");
+    else {
+        int min = p->data[1];
+        p->data[1] = p->data[p->cnt--];
+        int index = 1;
+        while (2 * index <= p->cnt && (p->data[index] > p->data[2 * index] || p->data[index] > p->data[2 * index + 1])) {
+            if (2 * index + 1 <= p->cnt) {
+                if (p->data[2 * index] < p->data[2 * index + 1]) {
+                    swap(&p->data[index], &p->data[2 * index]);
+                    index = 2 * index;
+                } else {
+                    swap(&p->data[index], &p->data[2 * index + 1]);
+                    index = 2 * index + 1;
+                }
+            } else break;
+        }
+        return min;
+    }
+}
+
+void printPqueue (pqueue *p) {
+    int index = 0;
+    for (index; index <= p->cnt; index++) {
+        printf("[%d: %d]", index, p->data[index]);
+    }
+}
+
+void main() {
+    pqueue sira1;
+    initialize(&sira1);
+    printf("Until you type \"-1\" your input will be added to priority queue:\n\n");
+    while(1) {
+        int input;
+        printf("\n-> ");
+        scanf("%d", &input);
+        if(input==-1) break;
+        else insert(&sira1, input);
+        printf("\n%d (added)\n", input);
+    }
+    printf("Until you type \"-1\", each input will remove the first element from the priority queue:\n\n");
+    while(1) {
+        int input;
+        printf("\n-> ");
+        scanf("%d", &input);
+        if(input==-1) break;
+        printf("\n%d (removed)\n", delete(&sira1));
+    }
+    printf("\n\n");
+    
+    printPqueue(&sira1);
+}
