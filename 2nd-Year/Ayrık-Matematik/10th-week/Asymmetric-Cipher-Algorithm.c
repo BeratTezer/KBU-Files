de38686f95ddcd1f88690214d1419ab46b3a2bf3
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2024-05-19 18:38:46 +0300

    Upload 19.5.24

---
+++ 2nd-Year/Ayrık-Matematik/10th-week/Asymmetric-Cipher-Algorithm.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// ----------------------------------------------
+// Bu kod bir Asimetrik (Açık Şifreli) şifreleme
+// algoritması çalışmasıdır. Her kullanıcı için
+// iki adet anahtar tanımlanır ve şifreleme buna
+// göre yapılır. Kullanıcı ileteceği mesajı alıcı
+// kişinin açık anahtarı ile şifreler ve alıcı
+// metni kendi gizli anahtari ile deşfire eder.
+// Berat Tezer                      19 Mayıs 2024
+// ----------------------------------------------
+
+int main() {
+    // Olduğunca işi hızlandıracak fonksiyonları kullan. Daha okunaklı temiz kod yaz. Öncekinden örnek alabilirsin.
+    
+    return 0;
+}
