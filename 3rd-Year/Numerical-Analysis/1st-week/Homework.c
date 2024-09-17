6f6f0fdff5979bcfa65bdc988d0c971805f521d9
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2024-09-17 15:35:36 +0300

    Update 17.9.24

---
+++ 3rd-Year/Numerical-Analysis/1st-week/Homework.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int det_calc(int rows, int columns, int matris[rows][columns]) {
+    if (rows != columns) printf("This function can only calculate the square matrises.");
+    else if (rows > 2) {
+        for (int i = 1; i < rows; i++) {
+
+        }
+    } else { // 2x2 matrix return case
+         
+    }
+}
+
+int main() {
+
+
+	return 0;
+}
