64fe3462600cd6c9ae63ea11bba0a702386c74b4
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2024-09-18 11:09:36 +0300

    Update Homework.c

--- 3rd-Year/Numerical-Analysis/1st-week/Homework.c
+++ 3rd-Year/Numerical-Analysis/1st-week/Homework.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+// This function specifically designated for using first rows of the matris by Berat Tezer
 int det_calc(int rows, int columns, int matris[rows][columns]) {
     if (rows != columns) printf("This function can only calculate the square matrises.");
     else if (rows > 2) {
-        for (int i = 1; i < rows; i++) {
-
+        int total=0;
+        for (int i = 1; i < columns; i++) {
+            int Minor[rows-1][columns-1];
+            for (int j=0; j<(rows-1); j++) { // Minor will be calculated like M[j][k] which is minor of matris[0][i]
+                for (int k=0; k<(columns-1); k++) {
+                    Minor[j][k] = matris[1][i+1];
+                }
+            }
+            return total += matris[0][i]*pow(-1,(1+i+1))*det_calc(j, k, Minor);
         }
+        return total;
     } else { // 2x2 matrix return case
          
     }
