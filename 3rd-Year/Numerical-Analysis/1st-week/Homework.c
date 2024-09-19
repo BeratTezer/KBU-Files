65e2e3d00ab15df5939b067337f9dc4d1c096904
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2024-09-19 11:54:31 +0300

    Update 19.9.24

--- 3rd-Year/Numerical-Analysis/1st-week/Homework.c
+++ 3rd-Year/Numerical-Analysis/1st-week/Homework.c
@@ -3,33 +3,35 @@
 
 // This function specifically designated for using first rows of the matris by Berat Tezer
 int det_calc(int rows, int columns, int matris[rows][columns]) {
-    if (rows != columns) printf("This function can only calculate the square matrises.");
-    else if (rows > 2) {
-        int total=0;
-        for (int i = 1; i < rows; i++) {
-            int j = 0
-            int Minor[rows-1][columns-1];
-            // Minor will be calculated like M[j][k] which is minor of matris[0][i]
-            for (int g=0; j<rows-1; j++) { 
-                for (int k=0; k<columns-1; k++) {
-                    Minor[g][k] = matris[i][j];
+    if (rows != columns) {
+        printf("This function can only calculate the determinant of square matrices.\n");
+        return 0;
+    } else if (rows == 2) {  // base case (2x2 matris)
+        return matris[0][0] * matris[1][1] - matris[0][1] * matris[1][0];
+    } else {
+        int total = 0; 
+        for (int i = 0; i < columns; i++) {
+            int Minor[rows - 1][columns - 1]; 
+            for (int j = 1, g = 0; j < rows; j++, g++) {
+                int k_minor = 0;
+                for (int k = 0; k < columns; k++) {
+                    if (k == i) continue; 
+                    Minor[g][k_minor] = matris[j][k];
+                    k_minor++;
                 }
-            }
-            j += 1;
+            }   
+            total += matris[0][i] * pow(-1, i) * det_calc(rows - 1, columns - 1, Minor);
         }
-        return total += matris[0][i]*pow(-1,(1+i+1))*det_calc(rows-1, columns-1, Minor);
-        return total;
-    } else { // 2x2 matrix return case
-        return matris[0][0]*matris[1][1]-matris[0][1]*matris[1][0];
+        return total;  
     }
 }
 
 int main() {
-    int matris[3][3];
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<3; j++) {
+    int matris[5][5];
+    for (int i=0; i<5; i++) {
+        for (int j=0; j<5; j++) {
             printf("[%d][%d]: ", i, j);
-            matris[i][j] = scanf("%d");
+            scanf("%d", &matris[i][j]);
         }
     }
 
