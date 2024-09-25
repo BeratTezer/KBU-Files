771f6176b3ab3372677ac2562e43662d8055c3a0
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2024-09-25 18:46:13 +0300

    Upload InClassActivities

---
+++ 3rd-Year/Visual-Programming/2nd-week/FirstConsApp/FirstConsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            Console.WriteLine("Enter Radius: ");
+            Console.ReadLine();
+
+            Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
