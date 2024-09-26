00ad9d16564491556bd4b9936a875a99f09fe271
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2024-09-26 12:56:08 +0300

    Upload 26.9.24

---
+++ 3rd-Year/Visual-Programming/2nd-week/ConsApp2/ConsApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsApp2
+{
+    class Bread
+    {
+        public int counter;
+    }
+
+    static class Butter
+    {
+        public static int counter;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
