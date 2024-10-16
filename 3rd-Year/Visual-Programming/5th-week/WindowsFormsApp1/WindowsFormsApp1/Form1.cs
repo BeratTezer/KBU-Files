7b6675f270303f3faf88cdd309598e2a0c0d353f
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2024-10-16 18:28:05 +0300

    In Class Assignment

--- 3rd-Year/Visual-Programming/5th-week/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ 3rd-Year/Visual-Programming/5th-week/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,6 +15,35 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        const double pi = 3.14;
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double r =  Convert.ToDouble(textBox1.Text);
+            double alan = pi * r * r;
+            label2.Text = Convert.ToString(alan);
+            label3.Text = "Dairenin alani " + Convert.ToString(alan);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double r = Convert.ToDouble(textBox1.Text);
+            double cevre = 2 * pi * r;
+            label2.Text = Convert.ToString(cevre);
+            label3.Text = "Dairenin cevresi " + Convert.ToString(cevre);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
