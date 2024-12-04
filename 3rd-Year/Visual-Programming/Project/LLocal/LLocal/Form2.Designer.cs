5c64ba6fbb5932b2d871ff349e8ccf4f12453ee7
Berat Tezer <64587561+BeratTezer@users.noreply.github.com>
2024-12-05 00:04:55 +0300

    Upload

--- 3rd-Year/Visual-Programming/Project/LLocal/LLocal/Form2.Designer.cs
+++ 3rd-Year/Visual-Programming/Project/LLocal/LLocal/Form2.Designer.cs
@@ -42,12 +42,12 @@
             // 
             // gradientBG1
             // 
-            this.gradientBG1.Angle = 93F;
+            this.gradientBG1.Angle = 44F;
             this.gradientBG1.Color0 = System.Drawing.Color.Blue;
             this.gradientBG1.Color1 = System.Drawing.Color.Orange;
             this.gradientBG1.Location = new System.Drawing.Point(-2, -5);
             this.gradientBG1.Name = "gradientBG1";
-            this.gradientBG1.Size = new System.Drawing.Size(957, 539);
+            this.gradientBG1.Size = new System.Drawing.Size(1389, 778);
             this.gradientBG1.TabIndex = 0;
             // 
             // Form2
@@ -55,12 +55,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(953, 534);
+            this.ClientSize = new System.Drawing.Size(1367, 770);
             this.Controls.Add(this.gradientBG1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
             this.Palette = this.kryptonPalette1;
             this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
 
         }
